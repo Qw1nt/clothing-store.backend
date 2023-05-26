5ca6686691de2630c9c5c9d3dbd6d7727c00405d
Рудных Павел <prudnykh@tmnbit.com>
2023-05-26 12:02:23 +0300

    Small changes

---
+++ ClothingStore.Data.Validators/AddCategoryRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using ClothingStore.Data.Requests;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace ClothingStore.Data.Validators;
+
+public class AddCategoryRequestValidator : AbstractValidator<AddCategoryRequest>
+{
+    public AddCategoryRequestValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotNull()
+            .NotEmpty();
+    }
+}
