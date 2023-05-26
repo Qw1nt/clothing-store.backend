1a19721142a0594eca59bfda8d411aca90a71491
Рудных Павел <prudnykh@tmnbit.com>
2023-05-26 13:13:59 +0300

    Update validator

--- ClothingStore.Data.Validators/AssignRoleToUserRequestValidator.cs
+++ ClothingStore.Data.Validators/AssignRoleToUserRequestValidator.cs
@@ -1,4 +1,5 @@
-﻿using ClothingStore.Data.Requests;
+﻿using ClothingStore.Configurations;
+using ClothingStore.Data.Requests;
 using FluentValidation;
 
 namespace ClothingStore.Data.Validators;
@@ -9,7 +10,11 @@ public class AssignRoleToUserRequestValidator : AbstractValidator<AssignRoleToUs
     {
         RuleFor(x => x.Role)
             .NotEmpty()
-            .NotNull();
+            .NotNull()
+            .Must(x => x 
+                is IdentityConfiguration.Roles.Admin
+                or IdentityConfiguration.Roles.Manager
+                or IdentityConfiguration.Roles.User);
 
         RuleFor(x => x.UserId)
             .NotNull()
